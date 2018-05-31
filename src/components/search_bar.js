1f69cb2e806a069d0087fcfe49e6320f04844be5
diogoresende <diogoresende10@gmail.com>
2018-05-31 16:13:39 -0300

    projeto curso react 31-05-2018 inicio search_bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return <input />; //React create element
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAZDqo0AC-ABKPZVrPZWBzLW1u0_cOSxO4';
+
 //Create a new component. This component shoud produce
 //some HTML
-const App = function() {
-    return <div>Hi!</div>;
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 //Take this component's generated HTML and put it
 //on the page (in the DOM)
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
